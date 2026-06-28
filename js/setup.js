fa79b3776635e959af97ff94341171f2e5b8b983
hallo123wert <79359578+hallo123wert@users.noreply.github.com>
2026-06-28 15:02:10 +0200

    Google optimierung (#64)

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,5 @@
-const pathname = window.location.pathname.replace(/\\/g, '/');
-const isMitgliederSubpage = pathname.includes('/mitglieder/') && /\/mitglieder\/[^/]+\//.test(pathname);
-const basePath = isMitgliederSubpage ? '../../' : pathname.includes('/random-stuff/') || pathname.includes('/mitglieder/') ? '../' : '';
-
 function loadSidebarStyles() {
-    const stylesheetHref = `${basePath}css/setup.css`;
+    const stylesheetHref = `/css/setup.css`;
     const existingStylesheet = document.querySelector(`link[rel="stylesheet"][href="${stylesheetHref}"]`);
 
     if (existingStylesheet) {
@@ -21,24 +17,24 @@ function loadSidebarStyles() {
 
 function generateSidebar() {
     const navItems = [
-        { emoji: '🏠', text: 'Startseite', href: 'index.html' },
-        { emoji: '👥', text: 'Mitglieder', href: 'mitglieder/mitglieder.html' },
-        { emoji: '🏫', text: 'Lehrer', href: 'lehrer.html' },
-        { emoji: '🎮', text: 'Games', href: 'games.html' },
-        { emoji: '🔢', text: 'Random Stuff', href: 'random-stuff.html' }
+        { emoji: '🏠', text: 'Startseite', href: '/index.html' },
+        { emoji: '👥', text: 'Mitglieder', href: '/mitglieder/mitglieder.html' },
+        { emoji: '🏫', text: 'Lehrer', href: '/lehrer.html' },
+        { emoji: '🎮', text: 'Games', href: '/games.html' },
+        { emoji: '🔢', text: 'Random Stuff', href: '/random-stuff.html' }
     ];
 
     const sidebarHTML = `
         <nav class="sidebar">
             <div class="sidebar-header">
-                <a href="${basePath}index.html" class="sidebar-link">
-                    <img class="sidebar-logo" src="${basePath}img/Logo.png">
+                <a href="/index.html" class="sidebar-link">
+                    <img class="sidebar-logo" src="/img/Logo.png">
                     <span class="sidebar-span">Menü</span>
                 </a>
             </div>
             <ul class="nav-links">
                 ${navItems.map(item => 
-                    `<li><a href="${basePath}${item.href}">${item.emoji} ${item.text}</a></li>`
+                    `<li><a href="${item.href}">${item.emoji} ${item.text}</a></li>`
                 ).join('')}
             </ul>
         </nav>
@@ -55,8 +51,8 @@ function generateFooter() {
     const footerHTML = `
         <footer>
             <ul>
-                <li><a href="${basePath}index.html">&copy; 2026 Team Wurstwasser</a></li>
-                <li><a href="${basePath}impressum.html">Impressum</a></li>
+                <li><a href="/index.html">&copy; 2026 Team Wurstwasser</a></li>
+                <li><a href="/impressum.html">Impressum</a></li>
             </ul>
         </footer>
     `;
