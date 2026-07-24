5a80cca761ca6dee53ae1af96976e12d57cbcf2f
Sebi608 <210533753+Sebi608@users.noreply.github.com>
2026-07-24 22:18:58 +0200

    Refactor fetchIp to fetchIps and improve error handling (#84)

--- js/my-ip.js
+++ js/my-ip.js
@@ -1,12 +1,28 @@
-function fetchIp() {
-  fetch('https://api64.ipify.org?format=json')
-    .then(response => response.json())
+async function fetchIps() {
+  const elemV4 = document.getElementById('my-ipv4');
+  const elemV6 = document.getElementById('my-ipv6');
+
+  fetch('https://api.ipify.org?format=json')
+    .then(res => res.json())
     .then(data => {
-      document.getElementById('my-ip').innerText = data.ip;
+      elemV4.innerText = data.ip;
     })
     .catch(() => {
-      document.getElementById('my-ip').innerText = 'Fehler beim Laden';
+      elemV4.innerText = 'Fehler beim Laden (IPv4)';
+    });
+
+  fetch('https://api64.ipify.org?format=json')
+    .then(res => res.json())
+    .then(data => {
+      if (data.ip.includes(':')) {
+        elemV6.innerText = data.ip;
+      } else {
+        elemV6.innerText = 'Keine IPv6-Verbindung vorhanden';
+      }
+    })
+    .catch(() => {
+      elemV6.innerText = 'Fehler beim Laden (IPv6)';
     });
 }
 
-fetchIp();
+fetchIps();
